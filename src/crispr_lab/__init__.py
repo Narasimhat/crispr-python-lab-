a32d36b3327433df25931eadc90bbf2ea8576e79
Narasimhat <52572352+Narasimhat@users.noreply.github.com>
2026-08-12 23:15:37 +0200

    Add src/crispr_lab/__init__.py

---
+++ src/crispr_lab/__init__.py
@@ -0,0 +1,5 @@
+"""Educational CRISPR sequence-analysis tools."""
+
+from .guides import GuideCandidate, find_spcas9_guides, reverse_complement
+
+__all__ = ["GuideCandidate", "find_spcas9_guides", "reverse_complement"]
